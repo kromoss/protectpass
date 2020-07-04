a61ee12257447ff825bf0125dbaaea6304246de4
Julian <katmanjulkia@gmail.com>
2020-07-04 11:44:40 +0200

    Ajoute deux (programmation modulaire)

--- learning.cpp
+++ learning.cpp
@@ -1,47 +1,15 @@
 #include <iostream>
-#include <cmath>
-#include <string>
-
+#include "addTwo.cpp"
 using namespace std;
 
 //test
-
-void dessineRectangle(int l, int h, char fill)
-{
-    if(l >= 0 && h >= 0)
-    {
-        for(int ligne = 0; ligne < h; ligne++)
-        {
-            for(int colonne = 0; colonne < l; colonne++)
-            {
-                cout << fill;
-            }
-            cout << endl;
-        }
-    }
-    else
-    {
-        cout << "La largeur et la hauteur doivent étre positif.";
-    }
-    
-}
-
 int main()
 {
     
-    int l = 0, h = 0;
-    char fill = '*';
+    int nb = 0;
 
-    cout << "Largeur : ";
-    cin >> l;
+    cout << "Tapez un nombre : ";
+    cin >> nb;
 
-    cout << "Hauteur : ";
-    cin >> h;
-
-    cout << "Remplissage : ";
-    cin >> fill;
-
-    dessineRectangle(l, h, fill);
-
-    return 0;
+    cout << nb << " + 2 = " << addTwo(nb);
 }
